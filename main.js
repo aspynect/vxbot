13ca0495c58c8a3519127f0a23e64833d2e9ee6f
PistachioPiper <peppypiper14@gmail.com>
2022-12-19 12:57:26 -0600

    vx

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ async function processMessage(message) {
         if (match.charAt(0) == '.' || match.charAt(0) == '/') {
             match = match.slice(1)
         }
-        responseContent = ''.concat("https://", responseContent, twitLinks[match], "\n")
+        responseContent = ''.concat("https://vx", responseContent, twitLinks[match], "\n")
     }
 
     let responseMessage = await message.reply(responseContent);
